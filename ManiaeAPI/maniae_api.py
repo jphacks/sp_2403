a9b7872578ef757835e4d3f9228d78cde98fdcf7
shinjinhakase <shinjinhakase314159@docomo.ne.jp>
2024-10-27 04:33:55 +0900

    データの形変わるパターンに対応

--- ManiaeAPI/maniae_api.py
+++ ManiaeAPI/maniae_api.py
@@ -19,20 +19,55 @@ class ManiaeAPI:
         routes = soup.find_all(class_="routeDetail")
 
         json_data = {}
-        for i,route in enumerate(routes):
+        for route_index,route in enumerate(routes):
+            # for element_index,element in enumerate(route.contents):
+            #     if not 'class' in element.attrs:
+            #         continue
+            #     data = {}
+            #     if element.attrs['class'] == ['station']:
+            #         data['kind'] = "station"
+            #         data['station'] = element.find("a").text
+            #         if sum(1 for e in elements if "station" in e.values()) == 0:
+            #             data['departure'] = element.find(class_="time").text
+            #         elif sum(1 for e in elements if "station" in e.values()) == len(route.find_all(class_="station")) - 1:
+            #             data['arrival'] = element.find(class_="time").text
+            #         else:
+            #             print(sum(1 for e in elements if "station" in e.values()))
+            #             data['arrival'] = element.find_all(class_="time")[0].find_all("li")[0].text[:-1]
+            #             data['departure'] = element.find_all(class_="time")[0].find_all("li")[1].text[:-1]
+            #     elif element.attrs['class'] == ['fareSection']:
+            #         if len(element.find_all(class_="station")) > 0:
+            #             station_data
+            #         data['kind'] = "fareSection"
+            #         data['transport'] = element.find(class_="transport").text
+            #     elif element.attrs['class'] == ['access', 'walk']:
+            #         data['kind'] = "walk"
+            #     elements.append(data)
             stations = []
             for station in route.find_all(class_="station"):
-            # for station in route.children:
                 data = {}
-                data["station"] = station.find("a").text
-                if len(station.find(class_="time")) == 1:
-                    data["departure"] = station.find(class_="time").text
-                elif len(station.find(class_="time")) == 2:
-                    data["arrival"] = station.find_all(class_="time")[0].find_all("li")[0].text[:-1]
-                    data["departure"] = station.find_all(class_="time")[0].find_all("li")[1].text[:-1]
+                data['kind'] = "station"
+                data['station'] = station.find("a").text
+                if len(stations) == 0:
+                    data['departure'] = station.find(class_="time").text
+                elif len(stations) == len(route.find_all(class_="station")) - 1:
+                    data['arrival'] = station.find(class_="time").text
+                else:
+                    data['arrival'] = station.find_all(class_="time")[0].find_all("li")[0].text[:-1]
+                    data['departure'] = station.find_all(class_="time")[0].find_all("li")[1].text[:-1]
                 stations.append(data)
-            route_index = "route" + str(i + 1)
-            json_data[route_index] = stations
+            transports = []
+            for tp in route.find_all(class_="transport"):
+                data = {}
+                data['kind'] = "transport"
+                data['transport'] = tp.text
+                transports.append(data)
+            route_number = "route" + str(route_index + 1)
+            json_data[route_number] = []
+            for i in range(0,len(transports)):
+                json_data[route_number].append(stations[i])
+                json_data[route_number].append(transports[i])
+            json_data[route_number].append(stations[-1])
 
         file = open('./output.json', 'w', encoding='utf-8')
         json.dump(json_data, file, ensure_ascii=False)
